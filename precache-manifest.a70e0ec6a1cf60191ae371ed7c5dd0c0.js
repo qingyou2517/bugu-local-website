6ae4523051bd56a0b39c4f021c6559bd9a8755ef
青游 <2517789608@qq.com>
2023-02-18 21:55:48 +0800

    deploy

---
+++ precache-manifest.a70e0ec6a1cf60191ae371ed7c5dd0c0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31bda7ca78bbf07a5aa9",
+    "url": "/bugu-local-website/css/app.8d9508dd.css"
+  },
+  {
+    "revision": "53edbde4514f7e79e118",
+    "url": "/bugu-local-website/css/chunk-vendors.44bb1968.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/bugu-local-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/bugu-local-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "48926b891925c97c5010ef750f109614",
+    "url": "/bugu-local-website/index.html"
+  },
+  {
+    "revision": "31bda7ca78bbf07a5aa9",
+    "url": "/bugu-local-website/js/app.f02f7719.js"
+  },
+  {
+    "revision": "53edbde4514f7e79e118",
+    "url": "/bugu-local-website/js/chunk-vendors.729c2a03.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/bugu-local-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bugu-local-website/robots.txt"
+  },
+  {
+    "revision": "74d57ec1aca01b959c7dd44f8f76d231",
+    "url": "/bugu-local-website/svg.js"
+  }
+]);
